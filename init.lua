f628d7f2ce9dc6a1d169930f361e2d266a126598
Justin Byers <justin.byers64@gmail.com>
2025-12-03 03:37:33 -0600

    Update tab settings
    - Set tab width to 4 spaces

--- init.lua
+++ init.lua
@@ -1019,3 +1019,7 @@ require('lazy').setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
