0b47de4391941f65c97b9a757b6d0c8dc1bb55d1
Justin Byers <justin.byers64@gmail.com>
2026-01-10 17:19:10 -0600

    Add transparency
    - Update NVIM theme with transparency settings for 80%

--- init.lua
+++ init.lua
@@ -896,8 +896,12 @@ require('lazy').setup({
     config = function()
       ---@diagnostic disable-next-line: missing-fields
       require('tokyonight').setup {
+        transparent = true,
         styles = {
           comments = { italic = false }, -- Disable italics in comments
+          sidebars = 'transparent',
+          floats = 'transparent',
+          -- transparent = true,
         },
       }
 
