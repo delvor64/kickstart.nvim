954a0d90d16f07163292965db5163bde898e05f6
Justin Byers <justin.byers64@gmail.com>
2026-01-19 00:39:05 -0600

    neo-tree node shortcuts
    - Add shorcut to expand all nodes

--- lua/custom/plugins/nvim-tree.lua
+++ lua/custom/plugins/nvim-tree.lua
@@ -2,11 +2,21 @@ return {
   {
     'nvim-neo-tree/neo-tree.nvim',
     branch = 'v3.x',
+    lazy = false,
     dependencies = {
       'nvim-lua/plenary.nvim',
       'MunifTanjim/nui.nvim',
-      'nvim-tree/nvim-web-devicons', -- optional, but recommended
+      'nvim-tree/nvim-web-devicons',
     },
-    lazy = false, -- neo-tree will lazily load itself
+    config = function()
+      require('neo-tree').setup {
+        window = {
+          mappings = {
+            ['z'] = 'close_all_nodes',
+            ['Z'] = 'expand_all_nodes',
+          },
+        },
+      }
+    end,
   },
 }
